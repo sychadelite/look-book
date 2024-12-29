5675ac450332e411cc0bfda0e278202af1dd1ddc
Mohammad Barj Lazuardi Dindashwara <hashiramakun58@gmail.com>
2024-12-30 04:00:10 +0700

    feat: install dotenv package

--- customers/src/main.ts
+++ customers/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const RABBITMQ_URL = 'RABBITMQ_URL';
+  const configService = app.get(ConfigService);
+  const RABBITMQ_URL = configService.get<string>('RABBITMQ_URL');
   app.enableCors();
   await app.listen(3000);
 
