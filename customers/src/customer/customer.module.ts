9bcd4037ca832c43494405bbf61deb0322073224
Mohammad Barj Lazuardi Dindashwara <hashiramakun58@gmail.com>
2024-12-30 05:19:30 +0700

    fix: resolve repository dependency on service constructor

--- customers/src/customer/customer.module.ts
+++ customers/src/customer/customer.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Customer])], // Register the Customer entity here
   providers: [CustomerService],
   controllers: [CustomerController]
 })
-export class CustomerModule {}
+export class CustomerModule { }
--- orders/src/order/order.module.ts
+++ orders/src/order/order.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order])], // Register the Order entity here
   providers: [OrderService],
   controllers: [OrderController]
 })
-export class OrderModule {}
+export class OrderModule { }
