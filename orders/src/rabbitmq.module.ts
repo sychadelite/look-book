a3eb93d21830621821e811d6b0d73c6444467eb9
Mohammad Barj Lazuardi Dindashwara <hashiramakun58@gmail.com>
2024-12-30 05:38:58 +0700

    fix: dynamic rabbitmq url

--- orders/src/rabbitmq.module.ts
+++ orders/src/rabbitmq.module.ts
@@ -1,41 +1,48 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientsModule, Transport, ClientProviderOptions } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'BOOK_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'RABBITMQ_URL',
-          ],
-          queue: 'book_queue',
-          queueOptions: { durable: false },
-        },
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService): Promise<ClientProviderOptions> => ({
+          name: 'BOOK_SERVICE',  // Add the name property
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],  // Dynamically get RABBITMQ_URL
+            queue: 'book_queue',
+            queueOptions: { durable: false },
+          },
+        }),
       },
       {
         name: 'CUSTOMER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'RABBITMQ_URL',
-          ],
-          queue: 'customer_queue',
-          queueOptions: { durable: false },
-        },
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService): Promise<ClientProviderOptions> => ({
+          name: 'CUSTOMER_SERVICE',  // Add the name property
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],  // Dynamically get RABBITMQ_URL
+            queue: 'customer_queue',
+            queueOptions: { durable: false },
+          },
+        }),
       },
       {
         name: 'ORDER_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'RABBITMQ_URL',
-          ],
-          queue: 'order_queue',
-          queueOptions: { durable: false },
-        },
+        inject: [ConfigService],
+        useFactory: async (configService: ConfigService): Promise<ClientProviderOptions> => ({
+          name: 'ORDER_SERVICE',  // Add the name property
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],  // Dynamically get RABBITMQ_URL
+            queue: 'order_queue',
+            queueOptions: { durable: false },
+          },
+        }),
       },
     ]),
   ],
